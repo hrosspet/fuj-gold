0a8652a056845c5677967b3e361e36f55cb3357c
hrosspet <hrosspet@gmail.com>
2020-03-28 18:12:04 +0100

    update config to 2020

--- fujgold/config.py
+++ fujgold/config.py
@@ -1,14 +1,14 @@
 CONFIG = {
-    'START_DATE': '05.03.2019',
+    "START_DATE": "28.03.2020",
     # 'END_DATE': '05.03.2018', # currently unused, but could be used as an upper limit
-    'GOOGLE': {
-        'SCOPES': 'https://www.googleapis.com/auth/spreadsheets',
-        'CLIENT_SECRET_FILE': 'client_secret.json',
-        'APPLICATION_NAME': 'Google Sheets API Python Quickstart',
-        'SHEET_NAME': 'Transfers bank',
-        'SPREADSHEET_ID': '1VIoZOvYjZXpy3AzO1E6yj-yNyBBzXywep8frB9pR7DY'
+    "GOOGLE": {
+        "SCOPES": "https://www.googleapis.com/auth/spreadsheets",
+        "CLIENT_SECRET_FILE": "client_secret.json",
+        "APPLICATION_NAME": "Google Sheets API Python Quickstart",
+        "SHEET_NAME": "Transfers bank",
+        "SPREADSHEET_ID": "1X659l4t3YtHc7dDkGLlbKQSLy6ZHFdrSGu0VIc-YuPc",
     },
-    'FIO': {
-        'REQUEST_STRING': 'https://www.fio.cz/ib2/transparent?a=2800359168&f=%s&t=%s'
-    }
+    "FIO": {
+        "REQUEST_STRING": "https://www.fio.cz/ib2/transparent?a=2800359168&f=%s&t=%s"
+    },
 }
