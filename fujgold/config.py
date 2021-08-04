a3a15ee8c218fcbec43cf46eb545053b1e9adc1f
konigkr <88429070+konigkr@users.noreply.github.com>
2021-08-04 11:01:44 +0200

    Update config.py

--- fujgold/config.py
+++ fujgold/config.py
@@ -1,12 +1,12 @@
 CONFIG = {
-    "START_DATE": "28.03.2020",
+    "START_DATE": "16.06.2021",
     # 'END_DATE': '05.03.2018', # currently unused, but could be used as an upper limit
     "GOOGLE": {
         "SCOPES": "https://www.googleapis.com/auth/spreadsheets",
         "CLIENT_SECRET_FILE": "client_secret.json",
         "APPLICATION_NAME": "Google Sheets API Python Quickstart",
         "SHEET_NAME": "Transfers bank",
-        "SPREADSHEET_ID": "1X659l4t3YtHc7dDkGLlbKQSLy6ZHFdrSGu0VIc-YuPc",
+        "SPREADSHEET_ID": "1Sj5i_CXAWDOBF0UjRAhaTuArKetiWWE0NoaKNeTc1Y0",
     },
     "FIO": {
         "REQUEST_STRING": "https://www.fio.cz/ib_api/rest/periods/oUTFke1wvtFXzbZ9kHNpe1wbdCxyWaqFInxBFcFzAXLFzFVjkSOJZGs4Gu3VaePH/%s/%s/transactions.csv"
